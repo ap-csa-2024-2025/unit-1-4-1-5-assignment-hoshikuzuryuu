5c6f633aa880d0164cfc07f8d0bff75e304d1d53
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-09-10 10:17:50 -0700

    whatthesus

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,10 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    double var = 3; 
+    System.out.println(var);
+
+    int val = 4.0;
+    System.out.println(val);
   }
 }
