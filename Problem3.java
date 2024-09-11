895babc3e9ee0f1b4a9908aa44a925ef266fac65
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-09-11 10:28:06 -0700

    LOG OUT NEXT TIME

--- Problem3.java
+++ Problem3.java
@@ -9,6 +9,9 @@ public class Problem3
     System.out.println("Your second digit is " + ((val%100)/10));
     System.out.println("Your third digit is " + (val%1000)/100);
     System.out.println("Your fourth digit is " + (val/1000));
+    System.out.println("The fifth digit left to go get the milk");
+    //PLEASE log out/RESTART the computer
+
   }
 }
 
